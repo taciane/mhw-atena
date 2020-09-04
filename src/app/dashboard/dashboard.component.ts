07ac3887ebe29d820fb4ab581d367ea425754232
Esther <esthercardososilva@gmail.com>
2020-09-03 23:57:35 -0300

    Mapeia imagem do dashboard - utiliza rwdimgmap para mapear os pontos de interesse da imagem principal do dashboard; - insere os material dialogs onclick nos pontos de interesse;

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,29 @@ import { WhatNextComponent } from './what-next/what-next.component';
 
 @Component({
   selector: 'app-dashboard',
-  // templateUrl: './dashboard.component.html',
-  templateUrl: './dashboard2.html',
+  templateUrl: './dashboard.component.html',
+  // templateUrl: './dashboard2.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  // ImageMap = require("image-map");
+  parts = [{
+    "shape": "circle",
+    "type": "objective",
+    "coords": "237,82,82"
+  }, {
+    "shape": "circle",
+    "type": "options",
+    "coords": "557,524,80"
+  }, {
+    "shape": "poly",
+    "type": "reality",
+    "coords": "342,258,350,248,495,247,504,263,504,397,496,408,355,408,343,399"
+  }, {
+    "shape": "poly",
+    "type": "next",
+    "coords": "339,655,346,643,485,643,499,654,499,790,491,804,347,802,339,787"
+  }];
 
   constructor(
     public dialog: MatDialog
@@ -66,5 +84,28 @@ export class DashboardComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {});
   }
+
+  partClicked(type) {
+    switch(type){
+      case 'objective':
+        this.openDialog();
+        break;
+
+      case 'options':
+        this.openOptionsDialog();
+        break;
+
+      case 'reality':
+        this.openRealityDialog();
+        break;
+
+      case 'next':
+        this.openWhatNextDialog();
+        break;
+
+      default:
+        break;
+    }
+ }
 
 }
